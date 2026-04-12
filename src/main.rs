281bb02670d6610e38b3a2cf8408f836ee07d706
Hongrui Fang <chfanghr@gmail.com>
2026-04-12 23:33:23 +0800

    test updates

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 fn main() -> Result<(), String> {
-    let p = toy::parse_program("main = let x = 69; y = 42 in s k i x y")?;
+    let p = toy::parse_program(
+        "
+        id x = x;
+        compose f g x = f (g x);
+        twice f = compose f f;
+        main = twice twice id 3
+    ",
+    )?;
     let mut machine = toy::template_instantiation::Machine::new(p)?;
     machine.eval()?;
-    // println!("{:#?}", machine);
+    println!("{:#?}", machine);
     println!("{:#?}", machine.stats);
     println!("{:#?}", machine.peak_node().borrow());
     Ok(())
